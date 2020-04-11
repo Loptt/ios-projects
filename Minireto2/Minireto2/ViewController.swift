17201e343c3c54741026b7a1dbb394cce4652efc
user168639 <user168639@74-80-239-57.ic2net.net>
2020-04-11 09:39:49 -0700

    Added minireto 2 project

---
+++ Minireto2/Minireto2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minireto2
+//
+//  Created by user168639 on 4/11/20.
+//  Copyright © 2020 user168639. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
